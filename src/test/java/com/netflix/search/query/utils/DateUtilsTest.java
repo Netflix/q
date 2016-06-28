b6c91bd17058e3be9010475ab1ef15c2dbd96584
Ivan Provalov <iprovalov@netflix.com>
2016-06-28 12:30:14 -0700

    date fix

--- src/test/java/com/netflix/search/query/utils/DateUtilsTest.java
+++ src/test/java/com/netflix/search/query/utils/DateUtilsTest.java
@@ -1,5 +1,7 @@
 package com.netflix.search.query.utils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.testng.Assert;
@@ -8,10 +10,12 @@ import org.testng.annotations.Test;
 public class DateUtilsTest {
 
 	@Test
-	public void dateTest()
+	public void dateTest() throws ParseException
 	{
 		DateUtil dateUtil = new DateUtil();
 		Date dateFromString = dateUtil.getDateFromString("2016Jun23_07:56:47");
-		Assert.assertEquals(dateFromString, new Date("Thu Jun 23 07:56:47 PDT 2016"));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss zzz");
+		Date expected = sdf.parse("23/06/2016 07:56:47 PDT");
+		Assert.assertEquals(dateFromString, expected);
 	}
 }
